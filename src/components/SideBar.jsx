44d6e79adcfaf8777662489feec7894685acbfc8
Aniki Yusuf <anikiyusuf99@gmail.com>
2023-04-19 14:32:12 +0100

    initial commit

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SideBar() {
   return (
           <div className="p-4  flex-col text-[#FFFFFF] items-start mt-6">
-        <div className="bg-[#45269C] rounded-lg  py-3 px-8 justify-center items-center 4xl:py-2 4xl:px-4 4xl:font-normal 4xl:text-sm"><svg  className="inline-block w-4" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div className="bg-[#45269C] rounded-lg  py-3 px-8 justify-center cursor-pointer tracking-widest items-center 4xl:py-2 4xl:px-4 4xl:font-normal 4xl:text-sm"><svg  className="inline-block w-4" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M24 12C24 12.2652 23.8946 12.5196 23.7071 12.7071C23.5196 12.8946 23.2652 13 23 13H13V23C13 23.2652 12.8946 23.5196 12.7071 23.7071C12.5196 23.8946 12.2652 24 12 24C11.7348 24 11.4804 23.8946 11.2929 23.7071C11.1054 23.5196 11 23.2652 11 23V13H1C0.734784 13 0.48043 12.8946 0.292893 12.7071C0.105357 12.5196 0 12.2652 0 12C0 11.7348 0.105357 11.4804 0.292893 11.2929C0.48043 11.1054 0.734784 11 1 11H11V1C11 0.734784 11.1054 0.48043 11.2929 0.292893C11.4804 0.105357 11.7348 0 12 0C12.2652 0 12.5196 0.105357 12.7071 0.292893C12.8946 0.48043 13 0.734784 13 1V11H23C23.2652 11 23.5196 11.1054 23.7071 11.2929C23.8946 11.4804 24 11.7348 24 12Z" fill="#FFFFFF"/>
 </svg>   Welcome Keerthi  </div>
 {/* Dashboard */}
@@ -53,7 +53,7 @@ export default function SideBar() {
 </svg> <p className="text-[#B8B9BD]"> Logout</p></div>
 </div>
 {/* New   Project */}
-       <div className="bg-[#703EFE] rounded-lg justify-center py-4 px-8 items-center flex gap-1 4xl:py-2 4xl:px-6">
+       <div className="bg-[#703EFE] rounded-lg cursor-pointer tracking-widest justify-center py-4 px-8 items-center flex gap-1 4xl:py-2 4xl:px-6">
           <svg  className="inline-block w-4" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M24 12C24 12.2652 23.8946 12.5196 23.7071 12.7071C23.5196 12.8946 23.2652 13 23 13H13V23C13 23.2652 12.8946 23.5196 12.7071 23.7071C12.5196 23.8946 12.2652 24 12 24C11.7348 24 11.4804 23.8946 11.2929 23.7071C11.1054 23.5196 11 23.2652 11 23V13H1C0.734784 13 0.48043 12.8946 0.292893 12.7071C0.105357 12.5196 0 12.2652 0 12C0 11.7348 0.105357 11.4804 0.292893 11.2929C0.48043 11.1054 0.734784 11 1 11H11V1C11 0.734784 11.1054 0.48043 11.2929 0.292893C11.4804 0.105357 11.7348 0 12 0C12.2652 0 12.5196 0.105357 12.7071 0.292893C12.8946 0.48043 13 0.734784 13 1V11H23C23.2652 11 23.5196 11.1054 23.7071 11.2929C23.8946 11.4804 24 11.7348 24 12Z" fill="#FFFFFF"/>
 </svg>
